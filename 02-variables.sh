5b603e45303f3e5a885d7088a8a2b275b803353f
santhan <santhanpeddaguravagari@gmail.com>
2025-10-11 13:18:05 +0530

    created the 02-variables.sh

---
+++ 02-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#this is the script for passing variables through arguments 
+#while executing the scripts we will pass the variable values as arguments
+greeting=$1
+name=$2
+echo "$greeting,$name" 
+
+echo "my name is $name"
