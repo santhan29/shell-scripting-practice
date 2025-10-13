69fafc7996e8d5a621b231cdfe339d0a65afe61f
santhan <santhanpeddaguravagari@gmail.com>
2025-10-13 16:05:41 +0530

    made significant changes to the script condition-2.sh

--- condition-2.sh
+++ condition-2.sh
@@ -6,8 +6,13 @@ userid=$(id -u)
 if [ $userid == 0 ]
 then 
     echo "user has superuser privileges"
-    dnf install nginx -y 
-
+    dnf installed nginx -y 
+    if [ $? == 0 ]
+    then 
+        echo "package is already installed .. no need to install"
+    else 
+        echo "package is installing"
+        dnf install nginx -y
 else
     echo "user has no superuser privileges"
 fi 
