0a148cdfa7475b38344be02b604c071977456cd4
santhan <santhanpeddaguravagari@gmail.com>
2025-10-17 22:23:37 +0530

    setting the debug mode

--- set.sh
+++ set.sh
@@ -2,7 +2,7 @@
 
 #this shell script demonstrates that whenever error occurs in a shell script the shell script will automatically exits and dont go for the execution of next commands for this purpose we are using the set -e command inside the linux 
 
-set -e # setting the automatic exit if we get error 
+set -ex # setting the automatic exit if we get error 
 
 echo "my name is santhan"
 echoo "what is your name"
