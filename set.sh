0a17f5275e469dcccf50ef737c5a4111d69dbf07
santhan <santhanpeddaguravagari@gmail.com>
2025-10-17 22:22:44 +0530

    new script for automatic exiting the code

---
+++ set.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#this shell script demonstrates that whenever error occurs in a shell script the shell script will automatically exits and dont go for the execution of next commands for this purpose we are using the set -e command inside the linux 
+
+set -e # setting the automatic exit if we get error 
+
+echo "my name is santhan"
+echoo "what is your name"
+echo "hi, my name is siva" 
