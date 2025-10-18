40cd812754613c954bd67c7148807beda41b49dd
santhan <santhanpeddaguravagari@gmail.com>
2025-10-18 11:47:24 +0530

    added new script for deleting the log files older than 14 days

---
+++ delete-old-log.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#this script demonstrates deleting the log files older than 14 days
+#algorithm
+---------------
+#first find the directory in which logs are stored and check whether that directory exists or not
+#find the log files in that directory  
+#then delete it  
+
+sour_dir=/home/ec2-user/log
+cd $sour_dir 
+if [ $? -ne 0 ]
+then 
+    echo "directory not exists"
+    exit 1 
+else
+    echo "directory exists" 
+fi 
