423d85596abcb707baad2af6ddc9793acffa8e61
santhan <santhanpeddaguravagari@gmail.com>
2025-10-18 16:08:32 +0530

    made some more changes to the backup-files.sh

--- backup-files.sh
+++ backup-files.sh
@@ -5,6 +5,7 @@
 source_dir=$1
 destination_dir=$2 
 days=${3:-14} #if $3 is empty, default is 14 days
+time_stamp=$(date +%Y-%m-%d-%H-%M-%S) 
 
 usage(){
     echo "usage:: sh backup-files.sh <source> <destination> <days(optional)>" 
@@ -29,9 +30,26 @@ files=$(find $source_dir -name "*.log" -mtime +14)
 
 echo "files: $files" 
 
-if [ ! -z $files ]  
+if [ ! -z $files ]  #true if files is empty, ! makes the expression false 
 then
     echo "files are found"
+    zip_file="$destination_dir/app-logs-$time_stamp.zip" 
+    find $source_dir -name "*.log" -mtime +14 | zip "$zip_file" -@ 
+
+    #check if zip file is successfully created or not 
+    if [ -f $zip_file ]
+    then 
+        echo "successfully zipped files older than $days"
+        #remove the files after zipping 
+        while IFS=  read -r line #IFS is internal field separator, here we are keeping ifs as empty, -r is for not to ignore special character like /
+        do 
+            echo "Deleting line: $file"
+            rm -rf $file 
+        done <<< $files 
+    else 
+        echo "zipping the files is failed"
+        exit 1 
+    fi 
 else 
     echo "no files older than $days"
 fi 
