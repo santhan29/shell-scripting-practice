15840509b8b6b3ba9646694bdb9482968fd9e835
santhan <santhanpeddaguravagari@gmail.com>
2025-10-22 11:34:42 +0530

    made some changes in the script for disk-usage

--- disk-usage.sh
+++ disk-usage.sh
@@ -5,5 +5,10 @@ disk_threshold=5 #in companies it will be 75
 
 while IFS=  read -r line #IFS is internal field separator, here we are keeping ifs as empty, -r is for not to ignore special character like /
 do 
-    echo "$line"  
+    USAGE=$(echo $line | grep xfs | awk -F " " '{print $6F}' | cut -d "%" -f1) 
+    PARTITION=$(echo $line | grep xfs | awk -F " " '{print $NF}') 
+    if [ $USAGE -ge $disk_threshold ] 
+    then 
+        echo "$PARTITION is more than $disk_threshold, current value is: $USAGE. PLEASE CHECK" 
+    fi
 done <<< $disk_usage 
